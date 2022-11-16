78cac131951d8c748e3ffcfc056d00b0a36883c4
anicegbem <matthewdlama4@gmail.com>
2022-11-16 20:00:24 +0100

    add getNumbers function

--- calculator.js
+++ calculator.js
@@ -1,7 +1,36 @@
+let numbers = document.getElementsByClassName('number');
+let array = Array.from(numbers);
+let result = document.getElementById('results')
+
 function addNumbers(a, b) {
     return a + b;
 }
 
-function subtract(a, b) {
+function subtractNumbers(a, b) {
     return a - b;
 }
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+function divideNumbers(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    return operator(a, b);
+}
+
+function getNumbers() {
+    array.forEach(number => 
+        number.addEventListener('click', function getValue(){
+            result.innerHTML = number.innerHTML;
+
+        })
+    );
+}
+
+getNumbers();
+
+// console.log(operate(multiplyNumbers, 5, 2));

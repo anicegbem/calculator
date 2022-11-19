59f5f5b6955196f4fbbe89afa460f7cdc67a209a
anicegbem <matthewdlama4@gmail.com>
2022-11-19 12:58:08 +0100

    add rest parameters

--- calculator.js
+++ calculator.js
@@ -1,29 +1,40 @@
 let numbers = document.getElementsByClassName('number');
 let array = Array.from(numbers);
 let result = document.getElementById('results');
+let clear = document.getElementById('clear');
 // result.textContent = "0"
 result.innerHTML = "0";
-// let a = "";
+let a = result.innerHTML;
 // result.textContent = "0";
 
-function addNumbers(a, b) {
-    return a + b;
+function addNumbers(...a) {
+    // use rest parameters, and then use the reduce() method to compute
+    // return a + b;
+    let items = [...a];
+    return items.reduce((a, b) => a + b, 0);
+    
 }
 
-function subtractNumbers(a, b) {
-    return a - b;
+function subtractNumbers(...a) {
+    let items = [...a];
+    return items.reduce((a, b) => a - b);
+    // return a - b;
 }
 
-function multiplyNumbers(a, b) {
-    return a * b;
+function multiplyNumbers(...a) {
+    let items = [...a];
+    return items.reduce((a, b) => a * b);
+    // return a * b;
 }
 
-function divideNumbers(a, b) {
-    return a / b;
+function divideNumbers(...a) {
+    let items = [...a];
+    return items.reduce((a, b) => a / b);
+    // return a / b;
 }
 
-function operate(operator, a, b) {
-    return operator(a, b);
+function operate(operator, ...a) {
+    return operator(...a);
 }
 
 function getNumbers() {
@@ -37,16 +48,32 @@ function getNumbers() {
             else if(result.innerHTML !== "0") {
                 result.innerHTML += number.innerHTML;
             }
+
+            a = result.innerHTML;
+
+            
             
 
             
         })
+        
+        
     );
 
-    // a = result.innerHTML;
+    
+
+    
+}
+
+function clearDisplay() {
+    clear.addEventListener('click', function removeValues() {
+        result.innerHTML = "0";
+    })
+
 }
 
 getNumbers();
+clearDisplay();
 // console.log(a);
 
 // console.log(operate(multiplyNumbers, 5, 2));

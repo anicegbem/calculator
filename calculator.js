11418ffd62f7df9e74d2089cbf52cc703a161fad
anicegbem <matthewdlama4@gmail.com>
2022-11-21 22:10:29 +0100

    fix trailing value error and fix display height

--- calculator.js
+++ calculator.js
@@ -5,6 +5,12 @@ let clear = document.getElementById('clear');
 // result.textContent = "0"
 result.innerHTML = "0";
 let a = result.innerHTML;
+let operators = document.getElementsByClassName('operator');
+let arrayOperators = Array.from(operators);
+let b = "";
+let equals = document.getElementById('equals');
+let up = document.getElementById('up');
+let value = "";
 // result.textContent = "0";
 
 function addNumbers(...a) {
@@ -60,6 +66,8 @@ function getNumbers() {
         
     );
 
+    replaceResults();
+
     
 
     
@@ -68,12 +76,108 @@ function getNumbers() {
 function clearDisplay() {
     clear.addEventListener('click', function removeValues() {
         result.innerHTML = "0";
+        up.innerHTML = "";
+        value = "";
     })
 
 }
 
-getNumbers();
-clearDisplay();
+function computeValues() {
+    arrayOperators.forEach(operator => 
+        operator.addEventListener('click', function getOperator() {
+            b = a + " " + operator.innerHTML + " ";
+            // array.forEach(number => 
+            //     number.addEventListener('click', function getSecondValue(){
+            //         result.innerHTML = number.innerHTML;
+            //         a = result.innerHTML
+
+            //     })
+            // )
+            // if(operator.innerHTML === "+") {
+            //     getSolution(addNumbers, a);
+
+            // }
+            // attach the first number plus the operator to the div above 
+            
+            up.innerHTML = b;
+
+        })
+    )
+    replaceResults();
+    
+
+}
+
+function getSolution(operator, a) {
+    equals.addEventListener('click', function getEqual() {
+        result.innerHTML = operate(operator, ...a);
+    })
+
+}
+
+function replaceResults() {
+    // change the value of 'a' after an operator has been selected
+    arrayOperators.forEach(operator => 
+        operator.addEventListener('click', function() {
+            array.forEach(number => number.addEventListener('click', function(){
+                /*let bArray = [...b];
+                let first = String(bArray[0]);*/
+                value += number.innerHTML;
+                 
+                
+                
+                
+                
+                
+    
+                // console.log(first.length);
+                // console.log(bArray[0]);
+                /*if(first.length < 2) {
+                    result.innerHTML = number.innerHTML;
+                }
+                else if (first.length >= 2) {
+                    result.innerHTML += number.innerHTML;
+                }*/
+
+                // result.innerHTML = a;
+
+            }))
+            // set value first to empty to prevent it from displaying on screen after clearing
+            value = "";
+            result.innerHTML = value;
+            
+            
+
+
+        })
+        
+       
+    )
+
+    clearDisplay();
+    
+    
+    
+
+}
+
+/*function addSecondValue(value) {
+    array.forEach(number => number.addEventListener('click', function() {
+        value += number.innerHTML;
+    }))
+}*/
+
+function calcValues() {
+    getNumbers();
+    clearDisplay();
+    replaceResults();
+    computeValues();
+    
+
+}
+
+calcValues();
+
 // console.log(a);
 
 // console.log(operate(multiplyNumbers, 5, 2));
